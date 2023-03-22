b2a45236e9fab62b393f40c303ae3e97371f878d
Roberto La Porta <r.laporta@bitrocket.dev>
2023-03-22 11:23:15 +0100

    fixup! chore(config): setup commitlint and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/** @format */
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -5,11 +7,26 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', '@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
   root: true,
   env: {
     node: true,
@@ -21,5 +38,40 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'no-unused-vars': 'error',
+    curly: 'error',
+    eqeqeq: 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/named': 'error',
+    'import/namespace': 'off',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-named-as-default-member': 'error',
+    'import/no-unresolved': 'error',
+    'import/no-unused-modules': 'error',
+    'import/prefer-default-export': 'off',
+    'no-alert': 'error',
+    'no-async-promise-executor': 'error',
+    'no-duplicate-imports': 'error',
+    'no-empty-function': 'off',
+    'no-empty': 'error',
+    'no-useless-escape': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prettier/prettier': [
+      'off',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
