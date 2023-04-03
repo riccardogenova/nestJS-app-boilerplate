3996e155c3ab6cc88689cdaf30aaa294f0a6d0d9
Roberto La Porta <100410178+robertolaporta-bitrocketdev@users.noreply.github.com>
2023-04-03 15:19:42 +0200

    feat(config): setup i18n service (#23)

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { jwtConfig } from './common/jwt.config';
 import { ExampleController } from './models/example/example.controller';
 import { ExampleModule } from './models/example/example.module';
 import { ExampleService } from './models/example/example.service';
+import { I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -19,7 +21,14 @@ import { ExampleService } from './models/example/example.service';
     // MongooseModule.forRoot(process.env.APP_MONGO_PATH),
     CacheModule.register(),
     JwtModule.registerAsync(jwtConfig),
-   ],
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+    }),
+  ],
   controllers: [AppController, ExampleController],
   providers: [AppService, ExampleService],
 })
