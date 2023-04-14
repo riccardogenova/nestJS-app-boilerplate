f90498335f91837a255d198bc0727629a2867bb8
Roberto La Porta <r.laporta@bitrocket.dev>
2023-04-14 14:15:01 +0200

    fix(config): changed bash configuration

--- config.sh
+++ config.sh
@@ -1,25 +1,30 @@
-echo Welcome to NestJs-boilerplate configuration.
-echo Which Database you want to use?:
-select opt in "MongoDB" "Postgres"; do
-      case $opt in
+echo #
+echo - Welcome to NestJs-boilerplate configuration -
+echo #
+echo - Choose a Database:
+echo #
+select opt in "MongoDB" "Postgres";
+do
+    case $opt in
         "MongoDB")
             rm -rf ./src/common/prisma-postgres
-            & mv ./src/common/prisma-mongodb ./src/common/prisma 
-            & rm ./docker-compose-postgres.yml
-            & mv ./docker-compose-mongodb.yml ./docker-compose.yml
-            && yarn
-            && rm ./config.sh
+            mv ./src/common/prisma-mongodb ./src/common/prisma 
+            rm ./docker-compose-postgres.yml
+            mv ./docker-compose-mongodb.yml ./docker-compose.yml
+            yarn
+            rm ./config.sh
             break
-            ;;
+             ;;
         "Postgres")
             rm -rf ./src/common/prisma-mongodb 
-            & mv ./src/common/prisma-postgres ./src/common/prisma
-            & rm ./docker-compose-mongodb.yml
-            & mv ./docker-compose-postgres.yml ./docker-compose.yml
-            && yarn && yarn remove mongoose mongodb @nestjs/mongoose
-            && rm ./config.sh
-            break
-            ;;
-        *) echo "Invalid option";;
+            mv ./src/common/prisma-postgres ./src/common/prisma
+            rm ./docker-compose-mongodb.yml
+            mv ./docker-compose-postgres.yml ./docker-compose.yml
+            yarn
+            yarn remove mongoose mongodb @nestjs/mongoose
+            rm ./config.sh
+             break
+             ;;
+         *) echo "Invalid option, choose 1 or 2...";;
     esac
 done
